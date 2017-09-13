bc9f854f0938de295a0a43222305c6b831bcba30
Lutz Hamel <lutz.hamel@gmail.com>
2017-09-13 16:23:57 -0400

    edits

--- Notebooks/code/cuppa1_pp.py
+++ Notebooks/code/cuppa1_pp.py
@@ -3,6 +3,7 @@
 
 from sys import stdin
 from cuppa1_frontend_gram import parser
+from cuppa1_lex import lexer
 from cuppa1_state import state
 from cuppa1_pp1_walk import walk as pp1_walk
 from cuppa1_pp2_walk import walk as pp2_walk
@@ -19,7 +20,7 @@ def pp(input_stream = None):
     init_indent_level()
 
     # build the AST
-    parser.parse(input_stream)
+    parser.parse(input_stream, lexer=lexer)
 
     # walk the AST
     pp1_walk(state.AST)
